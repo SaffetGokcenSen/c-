e2345eeb79ebedb422959908c0b2d46ad3b37fd8
Saffet Gokcen Sen <saffetgokcensen@hotmail.com>
2022-03-25 10:25:32 +0300

    tested the function for creating the rotations of the lines

---
+++ Accelerated-C-Plus-Plus/Chapter-5/Exercise-5-1/rotate_lines_tester.cpp
@@ -0,0 +1,40 @@
+#include "Process_lines.h"
+#include <iostream>
+#include <list>
+#include <string>
+
+using std::cout;
+using std::endl;
+using std::list;
+using std::string;
+using std::cin;
+
+int main() {
+    cout << "Please enter lines separated by newlines that can be created by"
+         << " pressing enter." << endl;
+    
+    list<string> input_lines;
+    read_lines(cin, input_lines);
+
+    cout << endl << endl << "The lines read by the program are as follows:" << endl;
+
+    for (list<string>::const_iterator it = input_lines.begin(); it != input_lines.end(); ++it) {
+        cout << *it << endl; 
+    }
+
+    list<Line_rotations> rotated_lines;
+    rotate_lines(input_lines, rotated_lines);
+
+    cout << endl << "The first words of the lines and their rotations are as follows:" << endl;
+    list<string> the_rotated_lines;
+    for (list<Line_rotations>::const_iterator it = rotated_lines.begin(); it != rotated_lines.end(); ++it) {
+        cout << it->firstWord;
+        cout << endl;
+        for (list<string>::const_iterator it2 = it->rotations.begin(); it2 != it->rotations.end(); ++it2) {
+            cout << *it2 << endl;
+        }
+        cout << "------------------------------" << endl;
+    }
+
+    return 0;
+}

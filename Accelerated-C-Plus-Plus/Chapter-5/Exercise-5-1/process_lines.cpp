590bd849442f5bf627471c08d13390deb62568e9
Saffet Gokcen Sen <saffetgokcensen@hotmail.com>
2022-03-25 10:25:01 +0300

    added the function for the creation of the list of structures holding the first word and the rotations of the inpu lines

--- Accelerated-C-Plus-Plus/Chapter-5/Exercise-5-1/process_lines.cpp
+++ Accelerated-C-Plus-Plus/Chapter-5/Exercise-5-1/process_lines.cpp
@@ -52,3 +52,49 @@ list<string> extract_words(const string& line) {
 
     return words_list;
 }
+
+list<Line_rotations>& rotate_lines(const list<string>& line_list, list<Line_rotations>& rotated_lines) {
+    // a line from the input list of lines
+    string the_line; 
+    // the definition of a type for constant iterators of string lists
+    typedef list<string>::const_iterator string_list_it;
+    // the list for the extracted words of the line from the input list of lines
+    list<string> word_list;
+    // each of the line structures to be accumulated in the rotated lines structure 
+    Line_rotations line_structure;
+    // the iterator for the input list of lines
+    string_list_it list_it;
+    // the string containing the rotated line
+    string rotated_line;
+    for (string_list_it it = line_list.begin(); it != line_list.end(); ++it) {
+        the_line = *it;
+        // the words of the line are stored in the word list
+        word_list = extract_words(the_line);
+        // an iterator is pointed to the beginning of the word list
+        list_it = word_list.begin();
+        // the first word of the line is stored in the first word element of the structure
+        line_structure.firstWord = *list_it;
+        for (list<string>::size_type i = 0; i < (word_list.size() - 1); ++i) {
+            rotated_line = "";
+            // send the first word to the end of the line
+            word_list.push_back(*list_it);
+            // erase the first word of the line
+            word_list.erase(list_it);
+            // point the iterator to the new beginning of the list
+            list_it = word_list.begin();
+            // combine the words of the rotated line
+            for (string_list_it iter = word_list.begin(); iter != word_list.end(); ++iter) {
+                rotated_line += *iter + " ";  
+            }
+            // the rotated line is stored in the rotations element of the structure
+            line_structure.rotations.push_back(rotated_line);
+        }
+        // the structure containing the first word and rotated versions of the line is stored
+        // in the rotated lines structure 
+        rotated_lines.push_back(line_structure);
+        // Remove the contents of the rotations element for the next line rotations
+        line_structure.rotations.clear();
+    }
+
+    return rotated_lines;
+}

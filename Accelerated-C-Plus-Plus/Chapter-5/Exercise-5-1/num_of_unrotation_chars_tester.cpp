64a52f0c22b665a3ae2ca8a6fa8a07a17936ec7f
Saffet Gokcen Sen <saffetgokcensen@hotmail.com>
2022-04-04 11:48:19 +0300

    tested the function for finding the number of chars for unrotating a rotated line

---
+++ Accelerated-C-Plus-Plus/Chapter-5/Exercise-5-1/num_of_unrotation_chars_tester.cpp
@@ -0,0 +1,63 @@
+#include "Process_lines.h"
+#include <iostream>
+#include <list>
+#include <string>
+#include <vector>
+
+using std::cout;
+using std::endl;
+using std::list;
+using std::string;
+using std::cin;
+using std::vector;
+
+int main() {
+    cout << "Please enter lines separated by newlines that can be created by"
+         << " pressing enter." << endl;
+    
+    list<string> input_lines;
+    read_lines(cin, input_lines);
+
+    cout << endl << endl << "The lines read by the program are as follows:" << endl;
+
+    for (list<string>::const_iterator it = input_lines.begin(); it != input_lines.end(); ++it) {
+        cout << *it << endl; 
+    }
+
+    list<Line_rotations> rotated_lines;
+    rotate_lines(input_lines, rotated_lines);
+
+    cout << endl << "The first and last words of the lines and their rotations are as follows:" << endl;
+    list<string> the_rotated_line;
+    list<string>::const_iterator it2;
+    for (list<Line_rotations>::const_iterator it = rotated_lines.begin(); it != rotated_lines.end(); ++it) {
+        cout << it->firstWord << " " << it->lastWord << endl;
+        the_rotated_line = it->rotation;
+        for (it2 = the_rotated_line.begin(); it2 != the_rotated_line.end(); ++it2) {
+            cout << *it2 << " ";
+        }
+        cout << endl;
+    }
+
+    rotated_lines.sort(string_compare);
+
+    cout << endl << "The rotated lines are listed in alphabetical order as follows:" << endl;
+    for (list<Line_rotations>::const_iterator it = rotated_lines.begin(); it != rotated_lines.end(); ++it) {
+        the_rotated_line = it->rotation;
+        for (it2 = the_rotated_line.begin(); it2 != the_rotated_line.end(); ++it2) {
+            cout << *it2 << " ";
+        }
+        cout << endl;
+    }
+
+    vector<string::size_type> char_num_vector;
+
+    char_num_vector = num_of_unrotation_chars(rotated_lines);
+
+    cout << endl << "The number of characters needed to unrotate the lines are as follows:" << endl;
+    for (int i = 0; i < char_num_vector.size(); ++i) {
+        cout << char_num_vector[i] << endl;
+    }
+
+    return 0;
+}

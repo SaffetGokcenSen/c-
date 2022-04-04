d9350117e5c45c604989eb36f83c9f13d4ce20d0
Saffet Gokcen Sen <saffetgokcensen@hotmail.com>
2022-04-04 11:22:37 +0300

    last_word element added to the structure, declaration of the function for finding the number of characters to unrotate a line

--- Accelerated-C-Plus-Plus/Chapter-5/Exercise-5-1/Process_lines.h
+++ Accelerated-C-Plus-Plus/Chapter-5/Exercise-5-1/Process_lines.h
@@ -5,6 +5,7 @@
 #include <list>
 #include <string>
 #include <iostream>
+#include <vector>
 
 std::istream& read_lines(std::istream&, std::list<std::string>&);
 
@@ -12,11 +13,20 @@ std::list<std::string> extract_words(const std::string&);
 
 struct Line_rotations {
     std::string firstWord;
+    std::string lastWord;
     std::list<std::string> rotation;
 };
 
 std::list<Line_rotations>& rotate_lines(const std::list<std::string>&, std::list<Line_rotations>&);
 
 bool string_compare(const Line_rotations, const Line_rotations);
+
+// determine the number of leading characters for each index word and
+// store them in an array of string sizes. Determine the maximum of
+// this array. Using existing rotated lines structure, unrotate each
+// line, front-pad each of them with a required number of blank spaces.
+// Add a fixed amount additional blank spaces before the index word. 
+// Write the index word and the rest.
+std::vector<std::string::size_type> num_of_unrotation_chars(const std::list<Line_rotations>&);
 
 #endif

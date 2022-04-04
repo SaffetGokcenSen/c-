e6b2cf32c343464b6e9165ec312ce93b5f1a5f67
Saffet Gokcen Sen <saffetgokcensen@hotmail.com>
2022-04-04 11:47:34 +0300

    updates du to the addition of the last_word element to the structure

--- Accelerated-C-Plus-Plus/Chapter-5/Exercise-5-1/sort_tester.cpp
+++ Accelerated-C-Plus-Plus/Chapter-5/Exercise-5-1/sort_tester.cpp
@@ -25,11 +25,11 @@ int main() {
     list<Line_rotations> rotated_lines;
     rotate_lines(input_lines, rotated_lines);
 
-    cout << endl << "The first words of the lines and their rotations are as follows:" << endl;
+    cout << endl << "The first and last words of the lines and their rotations are as follows:" << endl;
     list<string> the_rotated_line;
     list<string>::const_iterator it2;
     for (list<Line_rotations>::const_iterator it = rotated_lines.begin(); it != rotated_lines.end(); ++it) {
-        cout << it->firstWord << endl;
+        cout << it->firstWord << " " << it->lastWord << endl;
         the_rotated_line = it->rotation;
         for (it2 = the_rotated_line.begin(); it2 != the_rotated_line.end(); ++it2) {
             cout << *it2 << " ";

108ed30ff52f7b31f22be35ea91bec696bbb83bc
Saffet Gokcen Sen <saffetgokcensen@hotmail.com>
2022-04-13 13:59:45 +0300

    definitions for the read_lines and the center_picture functions

---
+++ Accelerated-C-Plus-Plus/Chapter-5/Exercise-5-5/Process_picture.h
@@ -0,0 +1,12 @@
+#ifndef GUARD_CENTER_PICTURE
+#define GUARD_CENTER_PICTURE
+
+#include <iostream>
+#include <list>
+#include <string>
+
+std::istream& read_lines(std::istream&, std::list<std::string>&);
+
+std::list<std::string> center_picture(std::list<std::string>&);
+
+#endif

362cadda6e54cd79ed402af1c5a4543e4004128d
Saffet Gokcen Sen <saffetgokcensen@hotmail.com>
2022-04-15 09:50:20 +0300

    added instructions for the user

--- Accelerated-C-Plus-Plus/Chapter-5/Exercise-5-5/main.cpp
+++ Accelerated-C-Plus-Plus/Chapter-5/Exercise-5-5/main.cpp
@@ -12,18 +12,24 @@ using std::endl;
 int main() {
     list<string> the_picture, centered_picture;
 
+    cout << "Please enter the character picture. Please finish your input with "
+         << "the end of file character which is ctrl+d in linux." << endl;
     read_lines(cin, the_picture);
 
     centered_picture = center_picture(the_picture);
 
+    cout << endl << endl << "The picture is printed with upper and lower "
+         << "asterisk lines of 80 characters:" << endl;
     list<string>::const_iterator it;
     string asterisks(80, '*');
-    cout << asterisks << endl;
+    cout << endl << asterisks << endl;
     for (it = the_picture.begin(); it != the_picture.end(); ++it) {
         cout << *it << endl;
     }
     cout << asterisks << endl;
-    
+
+    cout << endl << "The centered picture is printed with upper and lower "
+         << "asterisk lines of 80 characters:" << endl;
     cout << asterisks << endl;
     for (it = centered_picture.begin(); it != centered_picture.end(); ++it) {
         cout << *it << endl;

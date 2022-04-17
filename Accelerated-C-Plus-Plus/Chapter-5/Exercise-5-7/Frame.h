65d9dbdf34d9282466230ffdf6abf0520b190d0b
Saffet Gokcen Sen <saffetgokcensen@hotmail.com>
2022-04-17 15:39:03 +0300

    the functions width and frame are declared

---
+++ Accelerated-C-Plus-Plus/Chapter-5/Exercise-5-7/Frame.h
@@ -0,0 +1,11 @@
+#ifndef GUARD_FRAME
+#define GUARD_FRAME
+
+#include <string>
+#include <vector>
+
+std::string::size_type width(const std::vector<std::string>&);
+
+std::vector<std::string> frame(const std::vector<std::string>&);
+
+#endif

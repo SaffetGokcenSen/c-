b9da065166c9df0ff763ebfd7d26a748daebbc6b
Saffet Gokcen Sen <saffetgokcensen@hotmail.com>
2022-04-22 10:22:17 +0300

    defined the function letter_case_ordering

--- Accelerated-C-Plus-Plus/Chapter-5/Exercise-5-9/letter_case.cpp
+++ Accelerated-C-Plus-Plus/Chapter-5/Exercise-5-9/letter_case.cpp
@@ -40,3 +40,33 @@ bool uppercase_inclusion(const std::string& input_string) {
 
     return contains;
 }
+
+list<string> letter_case_ordering(std::list<std::string>& word_list) {
+  // indicates if it is the turn of the previously examined words
+  bool repeat = false;
+  // traces the word which has been first sent to the end of the word list
+  list<string>::size_type first_met_counter;
+  // indicates if any word has been sent to the end of the list
+  bool first_met = false;
+  // used to get the order of the word first sent to the end of the list
+  list<string>::size_type order = 1;
+  string current_word;
+  list<string>::const_iterator it = word_list.begin();
+  while ((!repeat) && (it != word_list.end())) {
+    current_word = *it;
+    if (uppercase_inclusion(current_word)) {
+      word_list.push_back(current_word);
+      if (!first_met) {
+        first_met = true;
+        first_met_counter = word_list.size() - order;
+      }
+      else {
+        --first_met_counter;
+      }
+    }
+    ++order;
+    repeat = (first_met_counter == 0);
+  }
+
+  return word_list;
+}

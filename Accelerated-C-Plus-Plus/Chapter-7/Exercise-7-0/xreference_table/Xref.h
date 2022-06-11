5650a9067c51af34cfcb34800ce822a2af691f53
Saffet Gokcen Sen <saffetgokcensen@hotmail.com>
2022-06-11 13:25:35 +0300

    added some comments

--- Accelerated-C-Plus-Plus/Chapter-7/Exercise-7-0/xreference_table/Xref.h
+++ Accelerated-C-Plus-Plus/Chapter-7/Exercise-7-0/xreference_table/Xref.h
@@ -12,8 +12,10 @@ bool space(char);
 // false if the argument is whitespace, true otherwise
 bool not_space(char);
 
+// finds the words separated by blanks in the input string
 std::vector<std::string> split(const std::string&);
 
+// find all the lines that refer to each word in the input
 std::map<std::string, std::vector<int> > xref(std::istream&,
 std::vector<std::string> find_words(const std::string&) = split);
 

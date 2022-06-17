74d7a7149959018985b7bd304bf9aafd3182e430
Saffet Gokcen Sen <saffetgokcensen@hotmail.com>
2022-06-17 13:54:49 +0300

    changed the sort_predicate function definition, defined the sort_word_count_pairs function to sort the contents of the input map

--- Accelerated-C-Plus-Plus/Chapter-7/Exercise-7-1/count_words.cpp
+++ Accelerated-C-Plus-Plus/Chapter-7/Exercise-7-1/count_words.cpp
@@ -3,10 +3,11 @@
 using std::map;
 using std::string;
 using std::istream;
+using std::list;
+using std::pair;
 
-// the predicate used to sort the elements of a list of pairs
-bool sort_predicate(const int& a, const int& b) {
-    return a < b;
+bool sort_predicate(const pair<string, int>& p1, const pair<string, int>& p2) {
+    return p1.second < p2.second;
 }
 
 // counts the words coming from the input stream
@@ -18,4 +19,22 @@ map<string, int> count_words(istream& in) {
     }
 
     return word_to_count;
+}
+
+// sorts the contents of the input map and outputs a list of pairs
+list<pair<string, int> > sort_word_count_pairs(const map<string, 
+int>& word_count_map) {
+    // contains the pairs of words and their occurrence counts
+    list<pair<string, int> > pairs_list;
+
+    // copies the pairs in the map to a list
+    for (map<string, int>::const_iterator it = word_count_map.begin(); 
+    it != word_count_map.end(); ++it) {
+        pairs_list.push_back(*it);
+    }
+
+    // sort the list of pairs according to the number of occurrences
+    pairs_list.sort(sort_predicate);
+
+    return pairs_list;
 }

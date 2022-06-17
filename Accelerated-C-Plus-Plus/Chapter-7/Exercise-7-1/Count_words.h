b24f5a3bd1581025a8580a88f45026960b4500a0
Saffet Gokcen Sen <saffetgokcensen@hotmail.com>
2022-06-17 11:58:50 +0300

    declared the function to determine the occurrence counts of words coming from an input stream

--- Accelerated-C-Plus-Plus/Chapter-7/Exercise-7-1/Count_words.h
+++ Accelerated-C-Plus-Plus/Chapter-7/Exercise-7-1/Count_words.h
@@ -1,7 +1,14 @@
 #ifndef GUARD_COUNT_WORDS
 #define GUARD_COUNT_WORDS
 
+#include <map>
+#include <string>
+#include <iostream>
+
 // the predicate used to sort the elements of a list of pairs
 bool sort_predicate(const int&, const int&);
+
+// counts the words coming from the input stream
+std::map<std::string, int > count_words(std::istream&);
 
 #endif

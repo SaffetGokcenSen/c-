cdce42ee197e04a1e4972fafd66981b064e10342
Saffet Gokcen Sen <saffetgokcensen@hotmail.com>
2022-06-17 12:31:34 +0300

    corrected the include statement, defined the function for counting word occurrences

--- Accelerated-C-Plus-Plus/Chapter-7/Exercise-7-1/count_words.cpp
+++ Accelerated-C-Plus-Plus/Chapter-7/Exercise-7-1/count_words.cpp
@@ -1,7 +1,21 @@
-#include <Count_words.h>
+#include "Count_words.h"
+
+using std::map;
+using std::string;
+using std::istream;
 
 // the predicate used to sort the elements of a list of pairs
 bool sort_predicate(const int& a, const int& b) {
     return a < b;
 }
 
+// counts the words coming from the input stream
+map<string, int> count_words(istream& in) {
+    map<string, int> word_to_count;
+    string the_word;
+    while(in >> the_word) {
+        ++word_to_count[the_word];
+    }
+
+    return word_to_count;
+}

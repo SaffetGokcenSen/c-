fc806f5406f34cabd7c2fa3ff9bf2698c8e32033
Saffet Gokcen Sen <saffetgokcensen@hotmail.com>
2022-06-17 13:08:55 +0300

    declared the function for sorting the contents of the word-occurrence count map

--- Accelerated-C-Plus-Plus/Chapter-7/Exercise-7-1/Count_words.h
+++ Accelerated-C-Plus-Plus/Chapter-7/Exercise-7-1/Count_words.h
@@ -4,11 +4,16 @@
 #include <map>
 #include <string>
 #include <iostream>
+#include <list>
 
 // the predicate used to sort the elements of a list of pairs
 bool sort_predicate(const int&, const int&);
 
 // counts the words coming from the input stream
 std::map<std::string, int> count_words(std::istream&);
+
+// sorts the contents of the input map and outputs a list of pairs
+std::list<std::pair<std::string, int> > 
+sort_word_count_pairs(const std::map<std::string, int>&);
 
 #endif

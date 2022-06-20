a1b191a792e5e9815b442ef1fee667d793768a2f
Saffet Gokcen Sen <saffetgokcensen@hotmail.com>
2022-06-20 16:10:14 +0300

    declared the function for determining the letter grades and storing them in a map

--- Accelerated-C-Plus-Plus/Chapter-7/Exercise-7-2/Letter_grades.h
+++ Accelerated-C-Plus-Plus/Chapter-7/Exercise-7-2/Letter_grades.h
@@ -3,7 +3,12 @@
 
 #include <list>
 #include <iostream>
+#include <map>
 
+// reads in grades
 std::list<double> read_grades(std::istream&);
+
+// determines the letter grades
+std::map<char, std::list<double> > get_letter_grades(const std::list<double>&);
 
 #endif

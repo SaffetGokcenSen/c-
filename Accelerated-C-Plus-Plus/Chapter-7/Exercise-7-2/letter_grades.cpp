b29f6a803e14f96a424e9e077372ce0896370788
Saffet Gokcen Sen <saffetgokcensen@hotmail.com>
2022-06-20 16:25:46 +0300

    defined the function get_letter_grades

--- Accelerated-C-Plus-Plus/Chapter-7/Exercise-7-2/letter_grades.cpp
+++ Accelerated-C-Plus-Plus/Chapter-7/Exercise-7-2/letter_grades.cpp
@@ -5,7 +5,9 @@ using std::istream;
 using std::cin;
 using std::cout;
 using std::endl;
+using std::map;
 
+// reads in grades
 list<double> read_grades(istream&) {
     // list of the grades
     list<double> grade_list;
@@ -22,4 +24,34 @@ list<double> read_grades(istream&) {
     }
 
     return grade_list;
+}
+
+// determines the letter grades and stores them in a map from letter grades to 
+// number grades
+map<char, list<double> > get_letter_grades(const list<double>& grade_list) {
+    // holds the distribution of the letter grades
+    map<char, list<double> > letter_grade_dist;
+
+    for (list<double>::const_iterator it = grade_list.begin(); 
+    it != grade_list.end(); ++it) {
+        double grade = *it;
+
+        if (grade < 60.0) {
+            letter_grade_dist['F'].push_back(grade);
+        }
+        else if ((grade >= 60.0) && (grade <= 69.99)) {
+            letter_grade_dist['D'].push_back(grade);
+        }
+        else if ((grade >= 70.0) && (grade <= 79.99)) {
+            letter_grade_dist['C'].push_back(grade);
+        }
+        else if ((grade >= 80.0) && (grade <= 89.99)) {
+            letter_grade_dist['B'].push_back(grade);
+        }
+        else if ((grade >= 90.0) && (grade <= 100.0)) {
+            letter_grade_dist['A'].push_back(grade);
+        }
+    }
+
+    return letter_grade_dist;
 }

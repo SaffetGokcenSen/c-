6dd9c53c2dc9303e0a41d2e38b88e0ee62ffc02b
Saffet Gokcen Sen <saffetgokcensen@hotmail.com>
2022-06-22 13:53:11 +0300

    defined the function create_xref

--- Accelerated-C-Plus-Plus/Chapter-7/Exercise-7-3/cross_reference.cpp
+++ Accelerated-C-Plus-Plus/Chapter-7/Exercise-7-3/cross_reference.cpp
@@ -1,9 +1,12 @@
 #include "Cross_reference.h"
+#include <algorithm>
 
 using std::list;
 using std::string;
 using std::istream;
 using std::getline;
+using std::map;
+using std::find;
 
 // the function for reading the lines
 list<string> read_lines(istream& in) {
@@ -43,4 +46,38 @@ list<string> extract_words(const string& line) {
     }
 
     return words_list;
+}
+
+// creates the cross-reference for the words
+map<string, list<int> > create_xref(const list<string>& list_of_lines) {
+    // the map from the words to their positions in terms of line numbers
+    map<string, list<int> > xref;
+    // holds a line
+    string line;
+    // line number
+    list<string>::size_type line_num = 1;
+
+    // get each line, split it into its words and create the xref
+    for (list<string>::const_iterator it = list_of_lines.begin(); 
+    it != list_of_lines.end(); ++it) {
+        line = *it;
+        list<string> words = extract_words(line);
+        for (list<string>::const_iterator it2 = words.begin(); 
+        it2 != words.end(); ++it2) {
+            string the_word = *it2;
+            if (!xref[the_word].empty()) {
+                // does the line number exist?
+                bool cond = xref[the_word].end() == 
+                find(xref[the_word].begin(), xref[the_word].end(), line_num);
+                // if the line number does not exist in the list, add it
+                if (cond) xref[the_word].push_back(line_num);
+            }
+            else {
+                xref[the_word].push_back(line_num);
+            }
+        }
+        ++line_num;
+    }
+
+    return xref;
 }

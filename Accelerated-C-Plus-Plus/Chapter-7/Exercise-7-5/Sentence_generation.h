31e39ff34ed5379f978b4661c2508338a08c9e60
Saffet Gokcen Sen <saffetgokcensen@hotmail.com>
2022-06-25 14:48:24 +0300

    declared the function for reading in the grammar from the input stream

--- Accelerated-C-Plus-Plus/Chapter-7/Exercise-7-5/Sentence_generation.h
+++ Accelerated-C-Plus-Plus/Chapter-7/Exercise-7-5/Sentence_generation.h
@@ -4,10 +4,14 @@
 #include <vector>
 #include <string>
 #include <map>
+#include <iostream>
 
 // custom type definitions for rule, category and grammar
 typedef std::vector<std::string> Rule;
 typedef std::map<std::string, std::vector<Rule> > Category;
 typedef std::map<Category, std::vector<Rule> > Grammar;
+
+// read the grammar from the input stream
+Grammar read_grammar(std::istream& in);
 
 #endif

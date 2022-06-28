229ab23ca55b7433cac863247433bb4c4c67a8a3
Saffet Gokcen Sen <saffetgokcensen@hotmail.com>
2022-06-28 15:50:51 +0300

    read_grammar function's part for reading in a valid category is complete

--- Accelerated-C-Plus-Plus/Chapter-7/Exercise-7-5/sentence_generation.cpp
+++ Accelerated-C-Plus-Plus/Chapter-7/Exercise-7-5/sentence_generation.cpp
@@ -33,12 +33,33 @@ list<string> extract_words(const string& line) {
 
 // read the grammar from the input stream
 Grammar read_grammar(std::istream& in) {
+    // the grammar read from the input stream
+    Grammar the_grammar;
     // prompt the user for entering the grammar
     cout << "Please enter the grammar line by line. Please quit your input with "
          << "the end-of-file character which is ctrl+d in linux.";
     
     string line;
     while (getline(in, line)) {
-
+        // words of the line
+        list<string> words = extract_words(line);
+        list<string>::const_iterator it = words.begin();
+        // first word of the line
+        string first_word = *it;
+        // if the first word is a valid category, add the rest of the line to the
+        // related category of the grammar map
+        if ((first_word[0] == '<') && (first_word.size() >= 3) && 
+        (first_word[first_word.size() - 1] == '>')) {
+            ++it;
+            Rule the_rule;
+            do {
+                the_rule.push_back(*it);
+                ++it;
+                }
+            while (it != words.end());
+            Category the_category;
+            the_category[first_word].push_back(the_rule);
+            the_grammar[first_word].push_back(the_category);
+        }
     }
 }

2d0ca475343649e105c4750195557c0cd91f2061
Saffet Gokcen Sen <saffetgokcensen@hotmail.com>
2022-07-08 14:22:15 +0300

    updated one of the parameters of the function sample_category

--- Accelerated-C-Plus-Plus/Chapter-7/Exercise-7-5/sentence_generation.cpp
+++ Accelerated-C-Plus-Plus/Chapter-7/Exercise-7-5/sentence_generation.cpp
@@ -100,7 +100,7 @@ int rand_int(int upper_bound) {
 
 //std::list<std::string> sample_category(const Grammar&, const std::string&);
 // generate a sample from a category entered by the user
-list<string> sample_category(const Grammar& the_grammar, const string& category,
+list<string> sample_category(Grammar& the_grammar, const string& category,
 list<string>& the_sample) {
     // typedef std::vector<std::string> Rule;
     // typedef std::map<std::string, std::vector<Rule> > Grammar;

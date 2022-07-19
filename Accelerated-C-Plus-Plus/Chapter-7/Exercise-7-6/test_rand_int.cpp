76ec2f63a961e8f1cad1d78e7cbb0347a54406c8
Saffet Gokcen Sen <saffetgokcensen@hotmail.com>
2022-07-19 10:37:05 +0300

    tested the function rand_int

---
+++ Accelerated-C-Plus-Plus/Chapter-7/Exercise-7-6/test_rand_int.cpp
@@ -0,0 +1,21 @@
+#include "Grammar.h"
+
+using std::cout;
+using std::endl;
+using std::cin;
+
+int main() {
+    cout << "Please enter an upper bound to generate 10 random integers less "
+         << "than this bound." << endl;
+    
+    int upper_bound;
+    cin >> upper_bound;
+    cout << endl;
+
+    for (int j = 0; j < 10; ++j) {
+        int random_int = rand_int(upper_bound);
+        cout << random_int << endl;
+    }
+
+    return 0;
+}

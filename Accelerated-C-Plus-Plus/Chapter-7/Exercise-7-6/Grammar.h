b8eb921124d1d7649e733daed3c0016cae1adb34
Saffet Gokcen Sen <saffetgokcensen@hotmail.com>
2022-07-19 10:09:51 +0300

    the declaration for the function read_grammar

--- Accelerated-C-Plus-Plus/Chapter-7/Exercise-7-6/Grammar.h
+++ Accelerated-C-Plus-Plus/Chapter-7/Exercise-7-6/Grammar.h
@@ -5,6 +5,7 @@
 #include <string>
 #include <map>
 #include <vector>
+#include <iostream>
 
 // custom type definitions for rule and grammar
 typedef std::list<std::string> Rule;
@@ -12,5 +13,8 @@ typedef std::map<std::string, std::vector<Rule> > Grammar;
 
 // splits the line into its words
 std::list<std::string> extract_words(const std::string&);
+
+// read the grammar from the input stream
+Grammar read_grammar(std::istream&);
 
 #endif

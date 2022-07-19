d6b04d9c4ef00954b035b04fbbdb7770e63e55c5
Saffet Gokcen Sen <saffetgokcensen@hotmail.com>
2022-07-19 10:11:57 +0300

    defined the function read_grammar

--- Accelerated-C-Plus-Plus/Chapter-7/Exercise-7-6/grammar.cpp
+++ Accelerated-C-Plus-Plus/Chapter-7/Exercise-7-6/grammar.cpp
@@ -2,6 +2,8 @@
 
 using std::list;
 using std::string;
+using std::cout;
+using std::endl;
 
 // split the line into its words
 list<string> extract_words(const string& line) {
@@ -26,4 +28,45 @@ list<string> extract_words(const string& line) {
     }
 
     return words_list;
+}
+
+// read the grammar from the input stream
+Grammar read_grammar(std::istream& in) {
+    // the grammar read from the input stream
+    Grammar the_grammar;
+    // prompt the user for entering the grammar
+    cout << "Please enter the grammar line by line. Please quit your input with "
+         << "the '|' character." << endl;
+    
+    string line;
+    while (getline(in, line)) {
+        // words of the line
+        list<string> words = extract_words(line);
+        list<string>::const_iterator it = words.begin();
+        // first word of the line
+        string first_word = *it;
+        if (first_word[0] == '|') {
+            return the_grammar;
+        }
+        else {
+            // if the first word is a valid category, add the rest of the line 
+            // to the related category of the grammar map
+            if ((first_word[0] == '<') && (first_word.size() >= 3) && 
+            (first_word[first_word.size() - 1] == '>')) {
+                ++it;
+                Rule the_rule;
+                do {
+                    the_rule.push_back(*it);
+                    ++it;
+                }
+                while (it != words.end());
+                the_grammar[first_word].push_back(the_rule);
+            }
+            else {
+                throw (first_word);
+            }
+        }
+    }
+    
+    return the_grammar;
 }

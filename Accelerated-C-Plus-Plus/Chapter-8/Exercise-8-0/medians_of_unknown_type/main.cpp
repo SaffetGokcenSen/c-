33a4b79878da78427399ff4875eb626e7fff91b3
Saffet Gokcen Sen <saffetgokcensen@hotmail.com>
2022-07-26 10:36:23 +0300

    written a program to implement the generic median function

---
+++ Accelerated-C-Plus-Plus/Chapter-8/Exercise-8-0/medians_of_unknown_type/main.cpp
@@ -0,0 +1,42 @@
+#include "Generic_median.h"
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::vector;
+using std::endl;
+
+int main() {
+    cout << "Please enter some numbers separated by newlines. These numbers "
+         << "should be either all integers or all reals. Please indicate the "
+         << "kind of the number as follows:" << endl
+         << "Please enter 'i' for integers." << endl
+         << "Please enter 'd' for reals." << endl
+         << "Please enter 'q' for quitting." << endl;
+    char the_choice;
+    while (cin >> the_choice) {
+        if (the_choice == 'i') {
+            vector<int> numbers;
+            int the_number;
+            while (cin >> the_number) {
+                numbers.push_back(the_number);
+            }
+            int the_median = median(numbers);
+            cout << "The median of the integers is " << the_median << endl;
+            return 0;
+        }
+        else if (the_choice == 'd') {
+            vector<double> numbers;
+            double the_number;
+            while (cin >> the_number) {
+                numbers.push_back(the_number);
+            }
+            double the_median = median(numbers);
+            cout << "The median of the reals is " << the_median << endl;
+            return 0;
+        }
+        else {
+            return 0;
+        }
+    }
+}

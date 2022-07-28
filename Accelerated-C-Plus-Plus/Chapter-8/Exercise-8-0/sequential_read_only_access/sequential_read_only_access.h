6889744325acf907ce9ba0688424f63176d4eabc
Saffet Gokcen Sen <saffetgokcensen@hotmail.com>
2022-07-28 20:57:25 +0300

    renamed this file

--- Accelerated-C-Plus-Plus/Chapter-8/Exercise-8-0/sequential_read_only_access/sequential_read_only_access.h
+++
@@ -1,7 +0,0 @@
-#ifndef GUARD_SEQUENTIAL_READ_ONLY_ACCESS
-#define GUARD_SEQUENTIAL_READ_ONLY_ACCESS
-
-template <class In, class X> In find(In begin, In end, const X& x);
-#include "sequential_read_only_access.hpp"
-
-#endif

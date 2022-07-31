430f2f133de43b5a13dc84615f4f80bd283c5ec9
Saffet Gokcen Sen <saffetgokcensen@hotmail.com>
2022-07-31 13:59:07 +0300

    declared the template function copy

---
+++ Accelerated-C-Plus-Plus/Chapter-8/Exercise-8-0/sequential_write_only_access/Sequential_write_only_access.h
@@ -0,0 +1,8 @@
+#ifndef GUARD_SEQUENTIAL_WRITE_ONLY_ACCESS
+#define GUARD_SEQUENTIAL_WRITE_ONLY_ACCESS
+
+template <class In, class Out>
+Out copy(In begin, In end, Out dest);
+#include "sequential_write_only_access.hpp"
+
+#endif

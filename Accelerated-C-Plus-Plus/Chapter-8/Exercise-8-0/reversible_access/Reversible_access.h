8d513900a6b5467ed47080b4a1c99d5a4498cbaa
Saffet Gokcen Sen <saffetgokcensen@hotmail.com>
2022-08-02 11:09:26 +0300

    included the source for the template function reverse

--- Accelerated-C-Plus-Plus/Chapter-8/Exercise-8-0/reversible_access/Reversible_access.h
+++ Accelerated-C-Plus-Plus/Chapter-8/Exercise-8-0/reversible_access/Reversible_access.h
@@ -4,4 +4,7 @@
 // declaration for the template function reverse
 template <class Bi> void reverse(Bi begin, Bi end);
 
+// the source for the template function reverse
+#include "reversible_access.hpp"
+
 #endif

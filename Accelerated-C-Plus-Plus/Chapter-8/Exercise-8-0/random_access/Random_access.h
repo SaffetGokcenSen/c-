2b80bbc971c5977fe9bf3aa25af3d9359ae3be3e
Saffet Gokcen Sen <saffetgokcensen@hotmail.com>
2022-08-02 11:49:21 +0300

    declared the template function binary_search

---
+++ Accelerated-C-Plus-Plus/Chapter-8/Exercise-8-0/random_access/Random_access.h
@@ -0,0 +1,8 @@
+#ifndef GUARD_RANDOM_ACCESS
+#define GUARD_RANDOM_ACCESS
+
+// declaration for the template function binary_search
+template <class Ran, class X>
+bool binary_search(Ran begin, Ran end, const X& x);
+
+#endif

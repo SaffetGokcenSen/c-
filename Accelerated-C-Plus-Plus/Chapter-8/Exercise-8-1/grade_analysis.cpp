3701ab04c176f86cee78f7c859dcb99fa6257000
Saffet Gokcen Sen <saffetgokcensen@hotmail.com>
2022-08-09 16:37:56 +0300

    defined the second grade function

--- Accelerated-C-Plus-Plus/Chapter-8/Exercise-8-1/grade_analysis.cpp
+++ Accelerated-C-Plus-Plus/Chapter-8/Exercise-8-1/grade_analysis.cpp
@@ -4,3 +4,8 @@ double grade(double midterm, double final, double homework)
 {
     return 0.2 * midterm + 0.4 * final + 0.4 * homework;
 }
+
+double grade(const Student_info& s)
+{
+    return grade(s.midterm, s.final, s.homework);
+}

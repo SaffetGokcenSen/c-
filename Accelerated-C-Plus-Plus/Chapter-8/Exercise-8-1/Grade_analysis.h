d07e7ff293a1d3c3e0fe1be9f5f70781f2017cf5
Saffet Gokcen Sen <saffetgokcensen@hotmail.com>
2022-08-09 16:21:19 +0300

    declared the function optimistic_median

--- Accelerated-C-Plus-Plus/Chapter-8/Exercise-8-1/Grade_analysis.h
+++ Accelerated-C-Plus-Plus/Chapter-8/Exercise-8-1/Grade_analysis.h
@@ -24,6 +24,8 @@ double average_grade(const Student_info&);
 
 double average_analysis(const std::vector<Student_info>&);
 
+double optimistic_median(const Student_info&);
+
 double optimistic_median_analysis(const std::vector<Student_info>&);
 
 template <class T, class X>

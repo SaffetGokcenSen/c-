faa6366223fa681236b9312f82dbab4531739d0c
Saffet Gokcen Sen <saffetgokcensen@hotmail.com>
2022-08-09 15:49:58 +0300

    declared the data structure Student_info

--- Accelerated-C-Plus-Plus/Chapter-8/Exercise-8-1/Grade_analysis.h
+++ Accelerated-C-Plus-Plus/Chapter-8/Exercise-8-1/Grade_analysis.h
@@ -2,8 +2,13 @@
 #define GUARD_GRADE_ANALYSIS
 
 #include <vector>
+#include <string>
 
-
+struct Student_info {
+    std::string name;
+    double midterm, final;
+    std::vector<double> homework;
+};
 
 template <class T, class X>
 X grade_analysis(const std::vector<T>& v, X x);

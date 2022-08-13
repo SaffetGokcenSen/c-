0cd4bf1a3e8dacf70f66dcc17d7e0093ff970ce4
Saffet Gokcen Sen <saffetgokcensen@hotmail.com>
2022-08-13 21:31:33 +0300

    defined the function median

--- Accelerated-C-Plus-Plus/Chapter-8/Exercise-8-1/grade_analysis.cpp
+++ Accelerated-C-Plus-Plus/Chapter-8/Exercise-8-1/grade_analysis.cpp
@@ -1,7 +1,10 @@
 #include "Grade_analysis.h"
 #include <stdexcept>
+#include <algorithm>
 
 using std::domain_error;
+using std::vector;
+using std::sort;
 
 double grade(double midterm, double final, double homework)
 {
@@ -20,4 +23,15 @@ double grade_aux(const Student_info& s)
     } catch (domain_error) {
         return grade(s.midterm, s.final, 0);
     }
+}
+
+double median(vector<double> vec)
+{
+    typedef vector<double>::size_type vec_sz;
+    vec_sz size = vec.size();
+    if (size == 0)
+        throw domain_error("median of an empty vector");
+    sort(vec.begin(), vec.end());
+    vec_sz mid = size/2;
+    return size % 2 == 0 ? (vec[mid] + vec[mid-1]) / 2 : vec[mid];
 }

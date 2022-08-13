440eb0672624a1431e711724ed88b39776163f3e
Saffet Gokcen Sen <saffetgokcensen@hotmail.com>
2022-08-13 23:36:44 +0300

    removed redundant function declarations

--- Accelerated-C-Plus-Plus/Chapter-8/Exercise-8-1/Grade_analysis.h
+++ Accelerated-C-Plus-Plus/Chapter-8/Exercise-8-1/Grade_analysis.h
@@ -18,17 +18,11 @@ double grade_aux(const Student_info&);
 
 double median(std::vector<double>);
 
-double median_analysis(const std::vector<Student_info>&);
-
 double average(const std::vector<double>&);
 
 double average_grade(const Student_info&);
 
-double average_analysis(const std::vector<Student_info>&);
-
 double optimistic_median(const Student_info&);
-
-double optimistic_median_analysis(const std::vector<Student_info>&);
 
 template <class T, class X>
 X grade_analysis(const std::vector<T>& v, X x);

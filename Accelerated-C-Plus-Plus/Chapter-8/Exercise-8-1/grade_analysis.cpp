1bbaff343b005db67b51c0d1f8020f045df3e3cc
Saffet Gokcen Sen <saffetgokcensen@hotmail.com>
2022-08-13 23:21:19 +0300

    defined the function average_analysis

--- Accelerated-C-Plus-Plus/Chapter-8/Exercise-8-1/grade_analysis.cpp
+++ Accelerated-C-Plus-Plus/Chapter-8/Exercise-8-1/grade_analysis.cpp
@@ -57,3 +57,10 @@ double average_grade(const Student_info& s)
 {
     return grade(s.midterm, s.final, average(s.homework));
 }
+
+double average_analysis(const vector<Student_info>& students)
+{
+    vector<double> grades;
+    transform(students.begin(), students.end(), back_inserter(grades), average_grade);
+    return median(grades);
+}

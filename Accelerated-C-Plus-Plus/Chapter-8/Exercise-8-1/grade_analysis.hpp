0286d1f662106440f0b88a9ebcc74c372ed940f6
Saffet Gokcen Sen <saffetgokcensen@hotmail.com>
2022-08-17 13:05:12 +0300

    updated the return type of the generic function grade_analysis

--- Accelerated-C-Plus-Plus/Chapter-8/Exercise-8-1/grade_analysis.hpp
+++ Accelerated-C-Plus-Plus/Chapter-8/Exercise-8-1/grade_analysis.hpp
@@ -7,7 +7,7 @@ using std::transform;
 using std::back_inserter;
 
 template <class T, class X>
-X grade_analysis(const std::vector<T>& v, X x) {
+double grade_analysis(const std::vector<T>& v, X x) {
     vector<double> grades;
     transform(v.begin(), v.end(), back_inserter(grades), x);
     return median(grades);

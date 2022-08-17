aa549ded932016019b9e24d5a61fc72f0812661a
Saffet Gokcen Sen <saffetgokcensen@hotmail.com>
2022-08-17 11:23:49 +0300

    defined the function read

--- Accelerated-C-Plus-Plus/Chapter-8/Exercise-8-1/grade_analysis.cpp
+++ Accelerated-C-Plus-Plus/Chapter-8/Exercise-8-1/grade_analysis.cpp
@@ -28,6 +28,15 @@ istream& read_hw(istream& in, vector<double>& hw)
     return in;
 }
 
+istream& read(istream& is, Student_info& s)
+{
+    // read and store the student’s name and midterm and final exam grades
+    is >> s.name >> s.midterm >> s.final;
+    // read and store all the student’s homework grades
+    read_hw(is, s.homework);
+    return is;
+}
+
 double grade(double midterm, double final, double homework)
 {
     return 0.2 * midterm + 0.4 * final + 0.4 * homework;

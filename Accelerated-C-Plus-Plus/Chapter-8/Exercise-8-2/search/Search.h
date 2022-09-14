fd15b737b7e8328e4c953439f7c15bd997915d2a
Saffet Gokcen Sen <saffetgokcensen@hotmail.com>
2022-09-14 16:28:17 +0300

    declared the functions space, not_space and the template function split

--- Accelerated-C-Plus-Plus/Chapter-8/Exercise-8-2/search/Search.h
+++ Accelerated-C-Plus-Plus/Chapter-8/Exercise-8-2/search/Search.h
@@ -1,9 +1,22 @@
 #ifndef GUARD_SEARCH
 #define GUARD_SEARCH
 
+#include <string>
+
 template <class For>
 For search(For, For, For, For);
 
 #include "search.hpp"
+
+// true if the argument is whitespace, false otherwise
+bool space(char);
+
+// false if the argument is whitespace, true otherwise
+bool not_space(char);
+
+template <class Out>
+void split(const std::string& str, Out os);
+
+#include "split.hpp"
 
 #endif

80e7a12ecd0c32b43de35c56e3b76842babd02c5
Saffet Gokcen Sen <saffetgokcensen@hotmail.com>
2022-09-16 16:32:53 +0300

    included the source for the template function find; declared the functions space, not_space; included the source for the template function split

--- Accelerated-C-Plus-Plus/Chapter-8/Exercise-8-2/find/Find.h
+++ Accelerated-C-Plus-Plus/Chapter-8/Exercise-8-2/find/Find.h
@@ -1,7 +1,22 @@
 #ifndef GUARD_FIND
 #define GUARD_FIND
 
+#include <string>
+
 template <class For, class T>
 For find(For, For, T);
+
+#include "find.hpp"
+
+// true if the argument is whitespace, false otherwise
+bool space(char);
+
+// false if the argument is whitespace, true otherwise
+bool not_space(char);
+
+template <class Out>
+void split(const std::string& str, Out os);
+
+#include "split.hpp"
 
 #endif

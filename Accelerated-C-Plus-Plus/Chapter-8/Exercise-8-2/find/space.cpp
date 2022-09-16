f52df2cbeee4cd97116a61ae654799912592d2f3
Saffet Gokcen Sen <saffetgokcensen@hotmail.com>
2022-09-16 16:52:12 +0300

    definitions of the functions space and not_space

---
+++ Accelerated-C-Plus-Plus/Chapter-8/Exercise-8-2/find/space.cpp
@@ -0,0 +1,14 @@
+#include "Find.h"
+#include <cctype>
+
+using std::isspace;
+
+// true if the argument is whitespace, false otherwise
+bool space(char c) {
+    return isspace(c);
+}
+
+// false if the argument is whitespace, true otherwise
+bool not_space(char c) {
+    return !isspace(c);
+}
